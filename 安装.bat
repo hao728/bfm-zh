cf12241fc2712e101eca6b58302985bd7c6afde8
hao728 <wkai42822@gmail.com>
2026-09-18 18:10:19 +0800

    Update 安装.bat

--- 安装.bat
+++ 安装.bat
@@ -69,26 +69,21 @@ if exist "%TARGET_DIR%\libcdio.dll" (
 )
 echo.
 
-echo [2/5] Checking 7-Zip...
+echo [2/5] Installing 7-Zip...
 set "SEVENZIP_DIR=Z:\opt\apps\7-Zip"
 if exist "%SEVENZIP_DIR%\7z.exe" (
-    echo       7-Zip found
-) else if exist "%SCRIPT_DIR%7z.exe" (
-    echo       Installing 7-Zip (portable)...
+    echo       7-Zip already installed, skip
+) else if exist "%SCRIPT_DIR%7-Zip\7z.exe" (
+    echo       Copying 7-Zip files...
     if not exist "%SEVENZIP_DIR%" mkdir "%SEVENZIP_DIR%"
-    copy /y "%SCRIPT_DIR%7z.exe" "%SEVENZIP_DIR%\" >nul
-    if exist "%SCRIPT_DIR%7z.dll" copy /y "%SCRIPT_DIR%7z.dll" "%SEVENZIP_DIR%\" >nul
+    xcopy /e /i /y "%SCRIPT_DIR%7-Zip\*" "%SEVENZIP_DIR%\" >nul
     if exist "%SEVENZIP_DIR%\7z.exe" (
         echo       7-Zip installed
     ) else (
         echo       [Warning] 7-Zip copy failed
     )
-) else if exist "%SCRIPT_DIR%7z*.exe" (
-    echo       Launching 7-Zip installer (please install manually to Z:\opt\apps\7-Zip)...
-    for %%f in ("%SCRIPT_DIR%7z*.exe") do start "" "%%f"
-    echo       [Info] 7-Zip installer opened, install it to Z:\opt\apps\7-Zip
 ) else (
-    echo       [Notice] 7-Zip not found, compression features will be limited
+    echo       [Notice] 7-Zip folder not found, compression features limited
 )
 echo.
 
@@ -123,6 +118,18 @@ if exist "%TARGET_DIR%\wfm.exe" (
 ) else (
     echo       [Warning] wfm.exe not found
 )
+echo.
+
+echo.
+
+echo.
+echo ============================================
+echo   Press any key to install 7-Zip...
+echo   Install path: Z:\opt\apps\7-Zip
+echo ============================================
+pause
+"%~dp07z2603-x64.exe"
+
 echo.
 echo ============================================
 echo   Install complete!
